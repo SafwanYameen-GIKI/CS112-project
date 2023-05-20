731976746c1ab51019c71729925e524f9ec71fc0
SafwanYameen-GIKI <u2022422@giki.edu.pk>
2023-05-20 21:54:41 +0500

    definition of pass and librarian member functions

--- project.cpp
+++ project.cpp
@@ -739,6 +739,78 @@ void Lib::student()
                 student();
             }
 }
+void Lib::pass()
+{
+    int i=0;
+    char ch,st[21],ch1[21]={"pass"};
+    cout<<"\n\t\tEnter Password : ";
+    while(1)
+    {
+    ch=getch();
+    if(ch==13)
+    {
+        st[i]='\0';
+        break;
+    }
+    else if(ch==8&&i>0)
+    {
+        i--;
+        cout<<"\b \b";
+    }
+    else
+    {
+    cout<<"*";
+    st[i]=ch;
+    i++;
+    }
+    }
+    ifstream inf("password.txt");
+    inf>>ch1;
+    inf.close();
+    for(i=0;st[i]==ch1[i]&&st[i]!='\0'&&ch1[i]!='\0';i++);
+    if(st[i]=='\0'&&ch1[i]=='\0')
+    {
+        system("cls");
+        librarian();
+    }
+    else
+    {
+        cout<<"\n\n\t\tWrong Password.\n\n\t\ttry again.....\n";
+        getch();
+        system("cls");
+        get();
+    }
+}
+void Lib::librarian()
+{
+    int i;
+        cout<<"\n\t************ WELCOME LIBRARIAN ************\n";
+        cout<<"\n\t\t>>Please Choose One Option:\n";
+        cout<<"\n\t\t1.View BookList\n\n\t\t2.Search for a Book\n\n\t\t3.Modify/Add Book\n\n\t\t4.Issue Book\n\n\t\t5.Go to main menu\n\n\t\t6.Change Password\n\n\t\t7.Close Application\n";
+        cout<<"\n\t\tEnter your choice : ";
+        cin>>i;
+        switch(i)
+        {
+            case 1:booklist(2);
+                     break;
+            case 2:see(2);
+                     break;
+            case 3:modify();
+                     break;
+            case 4:issue();
+                     break;
+            case 5:system("cls");
+                     get();
+                     break;
+            case 6:password();
+                    break;
+            case 7:exit(0);
+            default:cout<<"\n\t\tPlease enter correct option :(";
+            getch();
+            system("cls");
+            librarian();
+        }
+}
 int main()
 {
 
