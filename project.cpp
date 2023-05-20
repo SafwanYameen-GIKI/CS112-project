1b1a7ce547801299b97bf87e7994135d0631e8c0
SafwanYameen-GIKI <u2022422@giki.edu.pk>
2023-05-20 20:13:17 +0500

    creation of class Lib and its member functions

--- project.cpp
+++ project.cpp
@@ -5,6 +5,36 @@
 #include<string.h>
 #include<conio.h>
 using namespace std;
+class Lib
+{
+   public:
+       char bookname[100],auname[50],sc[20],sc1[50];
+       int q,B,p;
+       Lib()
+       {
+           strcpy(bookname,"NO Book Name");
+           strcpy(auname,"No Author Name");
+           strcpy(sc,"No Book ID");
+           strcpy(sc1,"No Book ID");
+           q=0;
+           B=0;
+           p=0;
+       }
+          void get();
+          void student();
+          void pass();
+          void librarian();
+          void password();
+          void getdata();
+          void show(int);
+          void booklist(int);
+          void modify();
+          void see(int);
+          int branch(int);
+          void issue();
+          void der(char[],int,int);
+          void fine(int,int,int,int,int,int);
+};
 
 int main()
 {
